e27dddab6425549c1c9ec7a08764a7652a815569
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2024-05-03 12:15:26 +0300

    SignalRKütüphanesi tamamlandı

---
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+
+namespace SignalRApi.Hubs
+{
+    public class SignalRHub:Hub
+    {
+        //    private readonly ICategoryService _categoryService;
+        //    private readonly IProductService _productService;
+        //    private readonly IOrderService _orderService;
+        //    private readonly IMoneyCaseService _moneyCaseService;
+        //    private readonly IMenuTableService _menuTableService;
+        //    private readonly IBookingService _bookingService;
+        //    private readonly INotificationService _notificationService;
+        //    public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService, INotificationService notificationService)
+        //    {
+        //        _categoryService = categoryService;
+        //        _productService = productService;
+        //        _orderService = orderService;
+        //        _moneyCaseService = moneyCaseService;
+        //        _menuTableService = menuTableService;
+        //        _bookingService = bookingService;
+        //        _notificationService = notificationService;
+        //    }
+
+
+        SignalRContext context = new SignalRContext();
+
+        public async Task SendCategoryCount()
+        {
+            var value = context.Categories.Count();
+
+            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+
+        }
+
+
+
+    }
+}
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -3,12 +3,26 @@ using SignalR.BusinessLayer.Concrete;
 using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.DataAccessLayer.EntityFramework;
+using SignalRApi.Hubs;
 using System.Reflection;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR();  
 
 
 builder.Services.AddDbContext<SignalRContext>();
@@ -60,10 +74,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
